ea9767b32e0c5a8ea0d11432c43610c896ea9daf
Jan Guardian <guardian.jan@gmail.com>
2023-02-21 19:36:55 +0300

    feat: Создан рендеринг карточк по-старинке

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ const cards = [
     heading: 'Карточка 3'
   },
 ]
+
+const page = document.querySelector('.page')
+
+cards.forEach(function(card) {
+  const cardHTML = `<div class="card">
+    <img src="${card.image}" class="card__image">
+    <div class="card__footer">
+      <span class="card__heading">${card.heading}</span>
+      <button class="card__button">Удалить</button>
+    </div>
+  </div>`
+  page.insertAdjacentHTML('beforeend', cardHTML)
+})
