797d8562c974ad7539088a5eb38c1bfd4be0b967
Jan Guardian <guardian.jan@gmail.com>
2023-02-21 12:38:52 +0300

    feat: Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const cards = [
+  {
+    image: 'https://picsum.photos/200?card=1',
+    heading: 'Карточка 1'
+  },
+  {
+    image: 'https://picsum.photos/200?card=2',
+    heading: 'Карточка 2'
+  },
+  {
+    image: 'https://picsum.photos/200?card=3',
+    heading: 'Карточка 3'
+  },
+]
