38e88b8c1bb5405827b4850fd4249f0724c8a7c5
Jan Guardian <guardian.jan@gmail.com>
2023-02-21 20:00:19 +0300

    refactor: Код создания карточки переведен на использование template-тега

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const cards = [
   {
     image: 'https://picsum.photos/200?card=1',
+    alt: 'Фотография молодой пары, гуляющей на пляже.',
     heading: 'Карточка 1'
   },
   {
     image: 'https://picsum.photos/200?card=2',
+    alt: '',
     heading: 'Карточка 2'
   },
   {
     image: 'https://picsum.photos/200?card=3',
+    alt: '',
     heading: 'Карточка 3'
-  },
+  }
 ]
 
 const page = document.querySelector('.page')
 
 cards.forEach(function(card) {
-  const cardHTML = `<div class="card">
-    <img src="${card.image}" class="card__image">
-    <div class="card__footer">
-      <span class="card__heading">${card.heading}</span>
-      <button class="card__button">Удалить</button>
-    </div>
-  </div>`
-  page.insertAdjacentHTML('beforeend', cardHTML)
+  const newCard = document.querySelector('#cardTemplate').content.cloneNode(true)
+  const cardHeading = newCard.querySelector('.card__heading')
+  cardHeading.textContent = card.heading
+  const cardImage = newCard.querySelector('.card__image')
+  cardImage.setAttribute('src', card.image)
+  cardImage.setAttribute('alt', card.alt)
+  page.append(newCard)
 })
